e6e171715a5f1724c5a433ae92bf9103b68b6d0a
David Sciacchettano <david.sciacchettano@gmail.com>
2020-04-01 20:21:27 -0400

    More work done

--- src/scrapper.ts
+++ src/scrapper.ts
@@ -1,36 +1,58 @@
-
-import { parse } from 'node-html-parser';
+import { MongoHelper } from './mongoclient';
 const axios = require('axios').default;
+import cheerio from 'cheerio'
 
 export class Scrapper {
     private root: string;
     private relativeUrl: string;
+    private mongoConnection: any;
 
     constructor(root:string, relativeUrl:string) {
         this.root = root;
         this.relativeUrl = relativeUrl;
+        this.mongoConnection = MongoHelper.connect('mongodb://localhost:27017/ocremix');
     }
 
-    public scrape = (): void => {
+    public scrape = async () => {
         let url;
-        for(let counter = 0; counter < 99999; counter++) {
+        let httpCalls = [];
+        for(let counter = 1; counter < 20; counter++) {
             url = this.getFullUrl(counter);
-            this.getOCRemixdata(url);
+            httpCalls.push(this.getOCRemixdata(url));
+            if(counter % 10 == 0) {
+                await Promise.all(httpCalls);                             
+                httpCalls.length = 0;
+            }
             console.log('Current URL:', url);
         }
     }
 
-    private getOCRemixdata = (url: string): void => {
-        const htl = axios.get(url)
-            .then(function(html: any)  {
-                console.log('HTML: ', html)
-            });
-
-
+    private getOCRemixdata = (url: string): Promise<any> => {
+        return axios.get(url)
+        .then((response: any) => {
+            this.parseHtml(response);
+        }).catch((err:any) => console.log('Failed to fetch URL: ', url));
     }
 
     private getFullUrl = (counter: number): string => {
         const ocremixNum = new String(counter);
-        return this.root + this.relativeUrl + ocremixNum.padStart(5, '0ya');
+        return this.root + this.relativeUrl + ocremixNum.padStart(5, '0');
+    }
+
+    private parseHtml = async (html: any): Promise<void> => {
+        
+        try{                   
+            console.log('\nHTTP status: ', html.status);
+            const parsedHtml = cheerio.load(html.data);
+           
+            if(html.status > 199 && html.status < 400) {
+                console.log('Valid JSON');
+                await this.mongoConnection.insert({'name':'test'});
+            } else {
+                console.log('Not a calid game URL: ', html);
+            }
+        }catch(e) {
+            console.log('Error: ', e);
+        }
     }
 }
